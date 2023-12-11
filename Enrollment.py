80256912456c5c8a2fd946b7f65db2c68165b5cb
jsanderswp <115518944+jsanderswp@users.noreply.github.com>
2023-12-10 18:58:21 -0800

    Update Enrollment.py

--- Enrollment.py
+++ Enrollment.py
@@ -10,3 +10,41 @@ from configparser import ConfigParser
 from datetime import time, datetime
 
 
+
+
+def add_student(db):
+    """
+    Add a new student, making sure that we don't put in any duplicates,
+    based on all the candidate keys (AKA unique indexes) on the
+    students collection.  Theoretically, we could query MongoDB to find
+    the uniqueness constraints in place, and use that information to
+    dynamically decide what searches we need to do to make sure that
+    we don't violate any of the uniqueness constraints.  Extra credit anyone?
+    :param collection:  The pointer to the students collection.
+    :return:            None
+    """
+    # Create a "pointer" to the students collection within the db database.
+    collection = db["students"]
+    unique_name: bool = False
+    unique_email: bool = False
+    lastName: str = ""
+    firstName: str = ""
+    email: str = ""
+    while not unique_name or not unique_email:
+        lastName = input("Student last name--> ")
+        firstName = input("Student first name--> ")
+        email = input("Student e-mail address--> ")
+        name_count: int = collection.count_documents(
+            {"last_name": lastName, "first_name": firstName}
+        )
+        unique_name = name_count == 0
+        if not unique_name:
+            print("We already have a student by that name.  Try again.")
+        if unique_name:
+            email_count = collection.count_documents({"e_mail": email})
+            unique_email = email_count == 0
+            if not unique_email:
+                print("We already have a student with that e-mail address.  Try again.")
+    # Build a new students document preparatory to storing it
+    student = {"last_name": lastName, "first_name": firstName, "e_mail": email}
+    results = collection.insert_one(student)
