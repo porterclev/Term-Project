41fa4a5663ba0e9b2004e424bb0669ffb43f6be7
jsanderswp <115518944+jsanderswp@users.noreply.github.com>
2023-12-13 19:13:59 -0800

    Update and rename lettergradeSchema.txt to lettergradeSchema.py

---
+++ lettergradeSchema.py
@@ -0,0 +1,29 @@
+def create_letter_grades_collection(db):
+  letter_grades: Collection = db["letter_grades"]
+  letter_grade_validator = 
+  {
+    'validator': {
+      '$jsonSchema': {
+        'bsonType': 'object',
+        'description': 'An LetterGrade enrollment of a student in a section.',
+        'required': [
+          'enrollment_id',
+          'min_satisfactory'
+        ],
+        'additionalProperties': False,
+        'properties': {
+          '_id': {},
+          'enrollment_id': {
+            'bsonType': 'integer',
+            'description': 'The ID number of the enrollment'
+          },
+          'min_satisfactory': {
+            'bsonType': 'string',
+            'enum': {'A','B','C','D','F'},
+            'description': 'The minimum satisfactory grade the student must achieve in the course'
+          }
+        }
+      }
+    }
+  }
+  db.create_collection("letter_grades", validator=letter_grade_validator)
