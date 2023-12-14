9659ee428ecece6ec6a1915a7114e1dd3f63c9b3
jsanderswp <115518944+jsanderswp@users.noreply.github.com>
2023-12-13 19:21:14 -0800

    Update passfailSchema.py

--- passfailSchema.py
+++ passfailSchema.py
@@ -10,7 +10,7 @@ def create_pass_fail_collection(db):
           'enrollment_id',
           'application_date'
         ],
-        'additionalProperties': false,
+        'additionalProperties': False,
         'properties': {
           '_id': {},
           'enrollment_id': {
