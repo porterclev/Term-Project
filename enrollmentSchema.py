00a9ac1bcd38bef5e6fc5b3cd595be72ffb4f6e6
jsanderswp <115518944+jsanderswp@users.noreply.github.com>
2023-12-13 19:11:46 -0800

    Update and rename enrollmentSchema.txt to enrollmentSchema.py

---
+++ enrollmentSchema.py
@@ -0,0 +1,34 @@
+def create_enrollments_collection(db):
+  enrollments: Collection = db["enrollments"]
+  enrollment_validator = 
+  {
+    'validator': {
+      '$jsonSchema': {
+        'bsonType': 'object',
+        'description': 'An enrollment of a student into a section.',
+        'required': [
+          'student_id',
+          'section_id'
+          'enrollment_type'
+        ],
+        'additionalProperties': False,
+        'properties': {
+          '_id': {},
+          'student_id': {
+            'bsonType': 'integer',
+            'description': 'The ID number of the student enrolling'
+          },
+          'section_id': {
+            'bsonType': 'integer',
+            'description': 'The ID number of the section the student is enrolling in'
+          },
+          'enrollment_type': {
+            'bsonType': 'string'
+            'enum': ['PassFail', 'LetterGrade'],
+            'description': 'The type of enrollment the student has in the section'
+          }
+        }
+      }
+    }
+  }
+  db.create_collection("enrollments", validator=enrollment_validator)
